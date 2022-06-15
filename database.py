181807f2bcc25fa5bb4a0bd77d68544b40cb7462
AugustineLove <augustinelovestephens@gmail.com>
2022-06-15 16:29:21 +0000

    commit of database

---
+++ database.py
@@ -0,0 +1,33 @@
+illness={
+    'Augustine': 'Malaria', 
+    'Asher': 'Fever',
+    'Elvis': 'Headache',
+    'Jerry': 'Stomach ache',
+    'Ben': 'Corona',
+    'Larry': 'Ulcer',
+    'Evelyn': 'Ebola',
+    'Esther': 'Diarrhoea',
+    'Aziz': 'Gonorrhea',
+    'Solomon': 'Toothache',
+    'Lucky':'Riverblindness',
+    'Franklin': 'Sorethroat',
+    'Michael': 'Hernia'        
+}
+
+
+ID={
+    'Augustine': '1122345',
+    'Asher': '1122346',
+    'Elvis': '1122347',
+    'Jerry': '1122348',
+    'Ben': '1122349',
+    'Larry': '1122350',
+    'Evelyn': '1122351',
+    'Esther': '1122352',
+    'Aziz': '1122353',
+    'Solomon': '1122354',
+    'Lucky': '1122355',
+    'Franklin': '1122356',
+    'Michael': '1122357'
+    }
+    
